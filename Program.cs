9a7f8ab1db070e246d847f3108b1c13c85d6e64a
blobular1 <davis_o@bryanston.co.uk>
2026-06-11 12:03:32 +0000

    not done

--- Program.cs
+++ Program.cs
@@ -4,8 +4,36 @@ class Program
 {
     static void Main()
     {
-        // Here is your empty program!
         Console.Clear();
-        Console.WriteLine("Hello world!");
+        Console.Write("Input string: ");
+        string key = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Input limit: ");
+        string limittemp = Console.ReadLine();
+        int limit = Convert.ToInt32(limittemp);
+
+        HashAlgorithm(key, limit);
+    }
+
+    static int HashAlgorithm(string key, int limit)
+    {
+        Console.WriteLine(key);
+        Console.WriteLine(limit);
+        int total = 0;
+        foreach (char c in key)
+        {
+            total += Convert.ToInt32(c);
+        }
+
+        total = total*total;
+        Console.WriteLine(total);
+        string digits = "";
+        digits += total[(total.Length())/2];
+        Console.WriteLine(digits);
+
+
+
+        int hash = 0;
+        return hash;
     }
 }
